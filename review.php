943715e5201e819196f0e3c878c72de7ef28e3ce
Don Slaughter <allenslaughter@me.com>
2016-12-08 13:51:24 -0500

    Initial Upload

---
+++ review.php
@@ -0,0 +1,22 @@
+<DOCTYPE! html>
+<head>
+	<title>Review</title>
+	<link rel="stylesheet" type="text/css" href="review.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+<div class="row">
+	<div class="col-4">
+		<div id="nav">
+			<ul>
+				<li><a class="active" href="#">Home</a></li>
+				<li><a href="#">Services</a></li>
+				<li><a href="#">Portfolio</a></li>
+				<li><a href="#">Contact</a></li>
+			</ul>
+		</div>
+	</div>
+</div>
+<script src="review.js"></script>
+</body>
+</html>
